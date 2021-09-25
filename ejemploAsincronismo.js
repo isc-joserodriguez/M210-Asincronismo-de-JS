ca262f7286f29b6d1099b24fac4978b6caa03392
José Rodriguez <isc.joserodriguez@gmail.com>
2021-09-25 11:26:31 -0600

    Ejemplo 1

---
+++ ejemploAsincronismo.js
@@ -0,0 +1,13 @@
+let array = [1, 2, 3]
+
+function leyendo() {
+    setTimeout(() => {
+        console.log(array);
+    }, 5000);
+}
+
+function leer(functionParam) {
+    functionParam();
+}
+
+leer(leyendo);
